1d0f31418a1b856d45520b863398034861d160b9
EC2 Default User <ec2-user@ip-172-31-0-97.ap-northeast-1.compute.internal>
2023-08-20 13:48:56 +0000

    アプリケーションを作ろう7章&8章バリデーションの設定まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,12 @@ class ListsController < ApplicationController
   end
   
   def create
-    list = List.new(list_params)
-    
-    list.save
-    
-    redirect_to list_path(list.id)
-    
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -33,6 +33,13 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destory
+    redirect_to '/lists'
+  end
+  
   
   private
   
